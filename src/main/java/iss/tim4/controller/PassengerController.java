e23aea843b40b4fa5aef5ee5d3eb4de446997c00
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2022-12-05 02:04:07 +0100

    Added more dtos for passenger

--- src/main/java/iss/tim4/controller/PassengerController.java
+++ src/main/java/iss/tim4/controller/PassengerController.java
@@ -1,6 +1,8 @@
 package iss.tim4.controller;
 
-import iss.tim4.domain.dto.PassengerDTO;
+import iss.tim4.domain.dto.PassengerDTORequest;
+import iss.tim4.domain.dto.PassengerDTOResponse;
+import iss.tim4.domain.dto.PassengerDTOResult;
 import iss.tim4.domain.model.Passenger;
 import iss.tim4.service.PassengerServiceJPA;
 import lombok.AllArgsConstructor;
@@ -22,37 +24,35 @@ public class PassengerController {
 
     // get all - /api/passenger
     @GetMapping
-    public ResponseEntity<List<PassengerDTO>> getPassengers() {
+    public ResponseEntity<PassengerDTORequest> getPassengers() {
         List<Passenger> passengers = passengerServiceJPA.findAll();
+        int totalCount = passengers.size();
 
-
-        // we have to convert passengers to passengers DTOs
-        List<PassengerDTO> passengerDTOS = new ArrayList<>();
-        for (Passenger p : passengers) {
-            passengerDTOS.add(new PassengerDTO(p));
+        PassengerDTOResult[] results = new PassengerDTOResult[totalCount];
+        for(int i = 0; i < passengers.size(); i++) {
+            results[i] = new PassengerDTOResult(passengers.get(i));
         }
-
-        return new ResponseEntity<>(passengerDTOS, HttpStatus.OK);
+        PassengerDTORequest passengerDTORequest = new PassengerDTORequest(results, totalCount);
+        return new ResponseEntity<>(passengerDTORequest, HttpStatus.OK);
 
     }
 
 
     // get by id - /api/passenger/1
     @GetMapping(value = "/{id}")
-    public ResponseEntity<PassengerDTO> getPassenger(@PathVariable("id") Integer id) {
+    public ResponseEntity<PassengerDTOResult> getPassenger(@PathVariable("id") Integer id) {
         Passenger passenger = passengerServiceJPA.findOne(id);
 
         if (passenger == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
-        return new ResponseEntity<PassengerDTO>(new PassengerDTO(passenger) , HttpStatus.OK);
+        return new ResponseEntity<PassengerDTOResult>(new PassengerDTOResult(passenger) , HttpStatus.OK);
     }
 
 
     // create /api/passenger
     @PostMapping(consumes = "application/json")
-    public ResponseEntity<PassengerDTO> createPassenger(@RequestBody PassengerDTO passengerDTO) throws Exception {
+    public ResponseEntity<PassengerDTOResponse> createPassenger(@RequestBody PassengerDTOResponse passengerDTO) throws Exception {
         Passenger passenger = new Passenger();
         passenger.setName(passengerDTO.getName());
         passenger.setSurname(passengerDTO.getSurname());
@@ -64,13 +64,13 @@ public class PassengerController {
         passenger.setActive(false);
         passenger.setBlocked(false);
         passenger = passengerServiceJPA.save(passenger);
-        return new ResponseEntity<>(new PassengerDTO(passenger), HttpStatus.CREATED);
+        return new ResponseEntity<>(new PassengerDTOResponse(passenger), HttpStatus.CREATED);
     }
 
     // update   --> /api/passenger/1
     // radi i ukoliko proslijedimo samo ona polja koja zelimo da promjenimo
     @PutMapping(value = "/{id}")
-    public ResponseEntity<PassengerDTO> updateDriver(@RequestBody PassengerDTO passengerDTO, @PathVariable Integer id)
+    public ResponseEntity<PassengerDTOResult> updatePassenger(@RequestBody PassengerDTOResponse passengerDTO, @PathVariable Integer id)
             throws Exception {
         Passenger passengerForUpdate = passengerServiceJPA.findOne(id);
         if (passengerForUpdate == null) {
@@ -99,13 +99,14 @@ public class PassengerController {
         if(passengerDTO.getEmail() != null) {
             passengerForUpdate.setEmail(passengerDTO.getEmail());
         }
-        passengerForUpdate = passengerServiceJPA.save(passengerForUpdate); // TODO:provjeri ovaj update
+
+        passengerForUpdate = passengerServiceJPA.save(passengerForUpdate);
 
         if (passengerForUpdate== null) {
-            return new ResponseEntity<PassengerDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<PassengerDTOResult>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return new ResponseEntity<PassengerDTO>(new PassengerDTO(passengerForUpdate), HttpStatus.OK);
+        return new ResponseEntity<PassengerDTOResult>(new PassengerDTOResult(passengerForUpdate), HttpStatus.OK);
     }
 
 
