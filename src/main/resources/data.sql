2fd74f47886961434623cbb430da2481933a9dff
Aleksander Mishutkin <106084948+AlexanderMishutkinP@users.noreply.github.com>
2022-12-11 21:21:03 +0100

    Fix some of bugs in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,107 +1,60 @@
-DELETE FROM PARTICIPATION;
-DELETE FROM ROUTE;
-DELETE FROM RIDE;
-DELETE FROM DRIVER_DOCUMENT;
-DELETE FROM DRIVER;
-DELETE FROM VEHICLE;
-DELETE FROM VEHICLE_TYPE;
-DELETE FROM LOCATION;
-DELETE FROM PASSENGER_ACTIVATION;
-DELETE FROM PASSENGER;
-DELETE FROM REJECTION;
-DELETE FROM USERS;
+INSERT INTO PASSENGER(active, address, blocked, email, name, password, profile_picture, surname, telephone_number)
+VALUES (true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20');
 
+INSERT INTO PASSENGER(id, active, address, blocked, email, name, password, profile_picture, surname, telephone_number)
+VALUES (123, true, 'NS, 21000', false, 'b@b.ru', 'Blex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 21');
 
+INSERT INTO PASSENGER_ACTIVATION (CREATION_DATE, LIFE_LENGTH, PASSENGER_ID)
+VALUES ('2022-10-10 10:21:20', 1, 1);
 
-INSERT INTO PASSENGER
-    (
-        id,
-        active,
-        address,
-        blocked,
-        email,
-        name,
-        password,
-        profile_picture,
-        surname,
-        telephone_number
-    ) VALUES
-        ( 1, true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20' ),
-        ( 123, true, 'NS, 21000', false, 'b@b.ru', 'Blex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 21' );
-
-INSERT INTO PASSENGER_ACTIVATION (ACTIVATION_ID, CREATION_DATE, LIFE_LENGTH, PASSENGER_ID) VALUES (1,  '2022-10-10 10:21:20', 1, 1);
-
-INSERT INTO LOCATION (ID, ADDRESS, GEO_LENGTH, GEO_WIDTH)
-    VALUES ( 1, 'NS 1', 54.6, 64.2 ),
-        ( 2, 'NS 2', 54.2, 64.3 ),
-        ( 3, 'NS 3', 54.4, 64.7 ),
-        ( 4, 'NS 4', 54.4, 64.4 ),
-        ( 5, 'NS 5', 54.8, 64.0 );
-
+INSERT INTO LOCATION (ADDRESS, GEO_LENGTH, GEO_WIDTH)
+VALUES ('NS 1', 54.6, 64.2),
+       ('NS 2', 54.2, 64.3),
+       ('NS 3', 54.4, 64.7),
+       ('NS 4', 54.4, 64.4),
+       ('NS 5', 54.8, 64.0);
 
 INSERT INTO VEHICLE_TYPE
-    (ID, PRICE, VEHICLE_NAME) VALUES ( 1, 200.00,  1), ( 2, 299.99,  2);
-
+    (PRICE, VEHICLE_NAME)
+VALUES (200.00, 1),
+       (299.99, 2);
 
 INSERT INTO VEHICLE
-    (ID, BABY_PROOF, MODEL, NUM_SEATS, PETS_ALLOWED, REG_PLATES, VEHICLE_NAME, LOCATION_ID) VALUES
-    ( 1, true, 'Tesla', 4, true,  'Sta?', 1, 1),
-    ( 2, false, 'Audi', 4, true,  'Sta??', 2, 2),
-    ( 3, true, 'Reno', 4, true,  'Sta???', 2, 3),
-    ( 4, true, 'Malinovaja Lada', 2, true,  'Sta????', 1, 4),
-    ( 5, false, 'Belarus', 1, true,  'Sta?????', 2, 5);
-
+(BABY_PROOF, MODEL, NUM_SEATS, PETS_ALLOWED, REG_PLATES, VEHICLE_NAME, LOCATION_ID)
+VALUES (true, 'Tesla', 4, true, 'Sta?', 1, 1),
+       (false, 'Audi', 4, true, 'Sta??', 2, 2),
+       (true, 'Reno', 4, true, 'Sta???', 2, 3),
+       (true, 'Malinovaja Lada', 2, true, 'Sta????', 1, 4),
+       (false, 'Belarus', 1, true, 'Sta?????', 2, 5);
 
 INSERT INTO DRIVER
-    (
-        ID,
-        ACTIVE,
-        ADDRESS,
-        BLOCKED,
-        EMAIL,
-        NAME,
-        PASSWORD,
-        PROFILE_PICTURE,
-        SURNAME,
-        TELEPHONE_NUMBER,
-        VEHICLE_ID
-    ) VALUES
-        ( 1, true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20', 1 ),
-        ( 2, true, 'NS, 21000 2', false, 'b@a.ru', 'Blex', 'kekekekekek', '2.png', 'Petrovicс', '+7 977 977 27 21', 2 ),
-        ( 3, true, 'NS, 21000 3', true, 'c@a.ru', 'Clex', 'kekekekekek', '3.png', 'Petrovicсс', '+7 977 977 27 22', 3 ),
-        ( 4, true, 'NS, 21000 4', false, 'd@a.ru', 'Dlex', 'kekekekekek', '3.png', 'Petroviccсс', '+7 977 977 27 23', 4 ),
-        ( 5, true, 'NS, 21000', false, 'e@y.ru', 'Elex', 'kekekekekek', '4.png', 'Petrovicсcсс', '+7 977 977 27 24', 5 ),
-        ( 123, true, 'NS, 21000', false, 'f@y.ru', 'Flex', 'kekekekekek', '4.png', 'Petrovicсcсс', '+7 977 977 27 25', 5 );
+(ACTIVE, ADDRESS, BLOCKED, EMAIL, NAME, PASSWORD, PROFILE_PICTURE, SURNAME, TELEPHONE_NUMBER, VEHICLE_ID)
+VALUES (true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20', 1),
+       (true, 'NS, 21000 2', false, 'b@a.ru', 'Blex', 'kekekekekek', '2.png', 'Petrovicс', '+7 977 977 27 21', 2),
+       (true, 'NS, 21000 3', true, 'c@a.ru', 'Clex', 'kekekekekek', '3.png', 'Petrovicсс', '+7 977 977 27 22', 3),
+       (true, 'NS, 21000 4', false, 'd@a.ru', 'Dlex', 'kekekekekek', '3.png', 'Petroviccсс', '+7 977 977 27 23', 4),
+       (true, 'NS, 21000', false, 'e@y.ru', 'Elex', 'kekekekekek', '4.png', 'Petrovicсcсс', '+7 977 977 27 24', 5);
 
-
-
+INSERT INTO DRIVER
+(ID, ACTIVE, ADDRESS, BLOCKED, EMAIL, NAME, PASSWORD, PROFILE_PICTURE, SURNAME, TELEPHONE_NUMBER, VEHICLE_ID)
+VALUES (123, true, 'NS, 21000', false, 'f@y.ru', 'Flex', 'kekekekekek', '4.png', 'Petrovicсcсс', '+7 977 977 27 25', 5);
 
 INSERT INTO USERS
-    (
-        id,
-        active,
-        address,
-        blocked,
-        email,
-        name,
-        password,
-        img_path,
-        surname,
-        phone
-    ) VALUES
-        ( 1, true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20' );
+(active, address, blocked, email, name, password, img_path, surname, phone)
+VALUES (true, 'NS, 21000', false, 'a@a.ru', 'Alex', 'kekekekekek', '1.png', 'Petrovic', '+7 977 977 27 20');
 
 
-INSERT INTO REJECTION (ID, REASON, TIME, USER_ID
-    ) VALUES
-    (1, 'Headache', '2022-11-11 10:32:01', 1);
+INSERT INTO REJECTION (REASON, TIME, USER_ID)
+VALUES ('Headache', '2022-11-11 10:32:01', 1);
 
 
 INSERT INTO RIDE
-    (ID, BABIES, END_TIME, ESTIMATED_TIME_IN_MINUTES, PANIC, PETS, START_TIME, STATUS, TOTAL_COST, DRIVER_ID, REJECTION_ID, VEHICLE_TYPE
-    ) VALUES
-        (1, true, '2022-10-10 10:21:20', 10.2, false, false, '2022-10-10 10:01:01', 0, 500, 1,1,2);
+(BABIES, END_TIME, ESTIMATED_TIME_IN_MINUTES, PANIC, PETS, START_TIME, STATUS, TOTAL_COST, DRIVER_ID, REJECTION_ID,
+ VEHICLE_TYPE)
+VALUES (true, '2022-10-10 10:21:20', 10.2, false, false, '2022-10-10 10:01:01', 0, 500, 1, 1, 2);
 
-INSERT INTO PARTICIPATION(RIDE_ID, PASSENGER_ID) VALUES (1, 1);
+INSERT INTO PARTICIPATION(RIDE_ID, PASSENGER_ID)
+VALUES (1, 1);
 
-INSERT INTO ROUTE (ID, KILOMETERS, END_LOCATION_ID, START_LOCATION_ID, PASSENGER_ID, RIDE_ID) values (1, 2.5, 1, 2, 1, 1);
+INSERT INTO ROUTE (KILOMETERS, END_LOCATION_ID, START_LOCATION_ID, RIDE_ID)
+values (2.5, 1, 2, 1);
