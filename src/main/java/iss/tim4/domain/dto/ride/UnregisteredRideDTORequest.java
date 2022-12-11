fb8466926b35e428f4c9dc5b747dc3df06c575ad
Aleksander Mishutkin <106084948+AlexanderMishutkinP@users.noreply.github.com>
2022-12-11 22:43:51 +0100

    Unregistered

---
+++ src/main/java/iss/tim4/domain/dto/ride/UnregisteredRideDTORequest.java
@@ -0,0 +1,18 @@
+package iss.tim4.domain.dto.ride;
+
+import iss.tim4.domain.VehicleName;
+import iss.tim4.domain.dto.RouteDTO;
+import iss.tim4.domain.dto.passenger.PassengerRideDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnregisteredRideDTORequest {
+    private Boolean babyTransport;
+    private Boolean petTransport;
+    private RouteDTO[] locations;
+    private VehicleName vehicleType;
+}
